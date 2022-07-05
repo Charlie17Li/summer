3356b3bba87752cbb195c25207d2536c80c7be9b
charlie <qianglin98@qq.com>
2022-07-05 17:28:58 +0800

    Support observe karmada components
    Signed-off-by: charlie <qianglin98@qq.com>

--- hack/deploy-prom.sh
+++ hack/deploy-prom.sh
@@ -4,14 +4,26 @@ set -e
 REPO_ROOT=$(dirname ${BASH_SOURCE[0]})/..
 source "$REPO_ROOT"/hack/util.sh
 
-KUBECONFIG=${KUBECONFIG:-"$HOME/.kube/config"}
-KUBECONTEXT=${KUBECONTEXT:-"$(kubectl config current-context --kubeconfig $KUBECONFIG)"}
+KUBECONFIG=${KUBECONFIG:-"$HOME/.kube/karmada.config"}
+KUBECONTEXT=${KUBECONTEXT:-"karmada-apiserver"}
+KARMADACONTEXT=${KUBECONTEXT:-"karmada-host"}
 
 # 创建ClusterRole 和 SA
+kubectl apply -f prometheus-rbac-setup.yaml --kubeconfig "$KUBECONFIG" --context "$KARMADACONTEXT"
+
+# 在Karmada-apiserver 创建ClusterRole 和 SA
 kubectl apply -f prometheus-rbac-setup.yaml --kubeconfig "$KUBECONFIG" --context "$KUBECONTEXT"
+pod_name=`kubectl get secret --context=karmada-apiserver | grep prometheus | awk '{print$1}'`
+token=`kubectl get secret "$pod_name" -o=jsonpath={.data.token} --context=karmada-apiserver | base64 -d`
+
+echo "token is" $token
+
+sed "s/karmada-token/$token/g" prometheus-config-template.yaml > prometheus-config.yaml
 
 # 创建Prom需要的配置
-kubectl apply -f prometheus-config.yaml --kubeconfig "$KUBECONFIG" --context "$KUBECONTEXT"
+kubectl apply -f prometheus-config.yaml --kubeconfig "$KUBECONFIG" --context "$KARMADACONTEXT"
 
 # 部署Prom
-kubectl apply -f prometheus-deployment.yaml --kubeconfig "$KUBECONFIG" --context "$KUBECONTEXT"
+kubectl apply -f prometheus-deployment.yaml --kubeconfig "$KUBECONFIG" --context "$KARMADACONTEXT"
+
+
--- hack/reset.sh
+++ hack/reset.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl apply -f prometheus-config.yaml
-kubectl get pod | grep prometheus | awk '{print$1}' | xargs kubectl delete pod
+kubectl apply -f prometheus-config.yaml --context karmada-host
+kubectl get pod --context karmada-host | grep prometheus | awk '{print$1}' | xargs kubectl delete pod --context karmada-host
